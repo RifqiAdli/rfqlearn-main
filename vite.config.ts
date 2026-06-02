cf42df446849f0127732d475a0acfa2be9999a5a
RifqiAdli <rifqiadli1212@gmail.com>
2026-06-02 21:47:38 +0700

    SSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  nitro: true, // <--- TAMBAHKAN BARIS INI
+  // Ubah bagian nitro menjadi seperti ini:
+  nitro: {
+    preset: "cloudflare-pages"
+  },
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts
     // nitro/vite builds from this
